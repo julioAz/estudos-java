65b07a86825e6976c790e28740a435630bafa83b
Julio Azevedo <cerebros.developer@gmail.com>
2021-12-09 20:40:16 -0300

    Tic Tac Toe

---
+++ src/com/company/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.company;
+
+//Escreva um programa em Java que permita 2 jogadores jogarem o clássico jogo da velha.
+//Não se esqueça de solicitar o nome de cada jogador e tornar o jogo interativo, alternando a vez
+// de jogar em o jogador 1 e o 2.
+//A cada jogada, verifique se o resultado do jogo, exiba o tabuleiro atualizado e informe, conforme o caso,
+// o nome do vencedor, o nome do jogador que irá jogar ou “Deu Velha!” caso não tenha mais opções de jogada.
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] tabuleiro = new char[3][3];
+        System.out.println(tabuleiro);
+    }
+}
